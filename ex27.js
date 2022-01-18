4e6b96226503e5e829b1a0d1827b8676e05f5175
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-01-17 23:48:06 -0300

    more

---
+++ ex27.js
@@ -0,0 +1,14 @@
+
+function createPhone(label="mootorola",valor=999,rate=5.0){
+    return{
+        label,
+        valor,
+        rate,
+
+    }
+}
+
+var newPhone = createPhone("samsung",1400,9.2)
+let count = 0
+
+console.log(newPhone)
